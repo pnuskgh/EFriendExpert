4f1c81bb36c4058f223277ed18129fd9028db67c
gye hyun james kim <pnuskgh@gmail.com>
2023-06-25 22:17:17 +0900

    docs: template 초안 작성

--- packages/EFriendExpert/templates/EFriendExport_api.ts
+++ packages/EFriendExpert/templates/EFriendExport_api.ts
@@ -6,8 +6,82 @@
  * @author gye hyun james kim <pnuskgh@gmail.com>
  */
 
+import fetch from "node-fetch";
+
+import { EFriendExpert_JSON } from "../../../docs/excel/EFriendExpert_json.js";
+
+import { 
+    RequestHeader, RequestHeaderAuth, RequestHeaderCommon,
+    RequestBody_Approval, ResponseBody_Approval,
+    RequestBody_hashkey, RequestBody_tokenP, RequestBody_revokeP
+} from "./EFriendExpert_type.ts";
+
+
 export class EFriendExport_API {
-    async _approval(trid: string, header: ppp, body: ppp): Promise<ppp> {
+    constructor() {
+        this._isProduct = true;
+    }
+
+    async _request(trid: string, header: RequestHeader, body: ppp): Promise<ppp> {
+        return {
+            header: {},
+            body: {}
+        };
+    }
+
+    async _delay() {
+        console.log('Reserved: _delay()');
+    }
+
+    async _Approval(trid: string, requestHeader: RequestHeader, requestBody: RequestBody_Approval): Promise<ResponseBody_Approval> {
+        await this._delay();
+
+        const info = EFriendExpert_JSON[trid].info;
+        const requestInfo = (this._isProduct) ? `${info.productionDomain}${info.url}`:`${info.developmentDomain}${info.url}`;
+        const requestInit = { 
+            method: into.method.toLowerCase(), 
+            mode: 'cors', 
+            cache: 'no-cache', 
+            headers: requestHeader
+        };
+        if (requestInit.method == 'post') {
+            requestInit.body = JSON.stringify(requestBody);
+        } else {
+            requestInfo = `${requestInfo}?${(new URLSearchParams(requestBody)).toString()}`;
+        }
+        const res = await fetch(requestInfo, requestInit);
+
+        //--- pppqqq
+
+
+        let approval_key = null;
+
+        const responseBody: ResponseBody_Approval = { approval_key: approval_key };
+        return responseBody;
+    }
+
+    async _hashkey(trid: string, header: RequestHeaderAuth, body: RequestBody_hashkey): Promise<ppp> {
+        return {
+            header: {},
+            body: {}
+        };
+    }
+
+    async _tokenP(trid: string, header: RequestHeader, body: RequestBody_tokenP): Promise<ppp> {
+        return {
+            header: {},
+            body: {}
+        };
+    }
+
+    async _revokeP(trid: string, header: RequestHeader, body: RequestBody_revokeP): Promise<ppp> {
+        return {
+            header: {},
+            body: {}
+        };
+    }
+
+    async _TTTC0802U(trid: string, header: RequestHeaderCommon, body: ppp): Promise<ppp> {
         return {
             header: {},
             body: {}
--- packages/EFriendExpert/templates/EFriendExport_type.ts
+++ packages/EFriendExpert/templates/EFriendExport_type.ts
@@ -6,22 +6,67 @@
  * @author gye hyun james kim <pnuskgh@gmail.com>
  */
 
-import { TR_CONT_CODE } from "./EFriendExpert_constant.ts";
+import { TR_CONT_CODE, CUSTTYPE_CODE } from "./EFriendExpert_constant.ts";
 
 export interface RequestHeader {
     contentType: 'application/json; charset=utf-8';
-    authorization: string;
+}
+
+export interface RequestHeaderAuth extends RequestHeader {
     appkey: string;
     appsecret: string;
-    personalseckey?: string | null;
+}
+
+export interface RequestHeaderDefault extends RequestHeaderAuth {
+    authorization: string;
     tr_id: string;
-    tr_cont: TR_CONT_CODE;
     custtype: CUSTTYPE_CODE;
+}
+
+export interface RequestHeaderCommon extends RequestHeaderDefault {
+    personalseckey?: string | null;
+    tr_cont: TR_CONT_CODE;
     seq_no?: string | null;
     mac_address?: string | null;
     phone_number?: string | null;
     ip_addr?: string | null;
     hashkey?: string | null;
     gt_uid?: string | null;
- }
- 
+}
+
+{% comment %} export interface RequestHeaderCorporation extends RequestHeaderDefault {
+    personalseckey: string;
+    tr_cont: TR_CONT_CODE;
+    seq_no: string;
+    mac_address?: string | null;
+    phone_number: string;
+    ip_addr: string;
+    hashkey?: string | null;
+    gt_uid: string;
+} {% endcomment %}
+
+export interface RequestBody_Approval {
+    grant_type: 'client_credentials',
+    appkey: string,
+    secretkey: string
+}
+
+export interface ResponseBody_Approval {
+    approval_key: string
+}
+
+export interface RequestBody_hashkey {
+    JsonBody: string
+}
+
+export interface RequestBody_tokenP {
+    grant_type: 'client_credentials',
+    appkey: string,
+    secretkey: string
+}
+
+export interface RequestBody_revokeP {
+    appkey: string,
+    secretkey: string
+    token: string
+}
