db26080fbe1621bdd9fc74320e5507945d562a53
Mountain Lover <pnuskgh@gmail.com>
2023-07-06 03:26:39 +0900

    env: TypeScript 실행 환경 구성

---
+++ modules/efriends/index.ts
@@ -0,0 +1,14 @@
+/**
+ * 한국투자증권 EFriendExpert의 main 프로그램
+ * 
+ * @file modules/main.ts
+ * @version 0.0.1
+ * @license GNU General Public License v3.0
+ * @copyright 2017~2023, EFriendExport Community Team
+ * @author gye hyun james kim <pnuskgh@gmail.com>
+ */
+
+export * from './efriend_json_trid';
+export * from './efriend_json_code';
+export * from './efriendError';
+export * from './efriendRest';
